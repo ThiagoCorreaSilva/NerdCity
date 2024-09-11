224318150bbdb5608985a999138644d6779bd41e
MushcV2 <titi.c2006@gmail.com>
2024-09-11 11:55:41 -0300

    Tower Rotation

--- Assets/Project/Scripts/TowerPlacing.cs
+++ Assets/Project/Scripts/TowerPlacing.cs
@@ -8,6 +8,8 @@ public class TowerPlacing : MonoBehaviour
     [SerializeField] private GameObject towerPrefab;
     [SerializeField] private GameObject towerSpawned;
     [SerializeField] private Button putTowerButton;
+    private bool alreadyPlaced;
+    private int rot = 0;
 
     private void Start()
     {
@@ -24,12 +26,36 @@ public class TowerPlacing : MonoBehaviour
         {
             if (_hit.collider.gameObject.layer == LayerMask.NameToLayer("Placeabled"))
             {
-                Debug.Log("Da para colocar");
-                towerSpawned.transform.position = new Vector3(_hit.point.x, 2.5f, _hit.point.z);
+                towerSpawned.transform.position = new Vector3(_hit.point.x, 0.05f, _hit.point.z);
+                alreadyPlaced = false;
+
+                Debug.Log("Pode colocar");
+            }
+            else
+            {
+                alreadyPlaced = true;
+                Debug.Log("Nao pode colocar");
             }
         }
 
-        if (Input.GetButtonDown("Fire1")) towerSpawned = null;
+        if (Input.GetButtonDown("Fire1") && alreadyPlaced)
+        {
+            towerSpawned.GetComponent<TowerAttack>().canAttack = true;
+            towerSpawned = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rot += 90;
+            towerSpawned.transform.rotation = Quaternion.Euler(0, rot, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.X) && towerSpawned != null)
+        {
+            Destroy(towerSpawned);
+            towerSpawned = null;
+        }
+
     }
 
     private void PutTower()
